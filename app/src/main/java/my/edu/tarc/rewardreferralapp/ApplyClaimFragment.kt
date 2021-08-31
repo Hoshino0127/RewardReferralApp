d779cae8b142e7b7eff85bef6a2e0a6a2f783a2f
Hoshino0127 <66240969+Hoshino0127@users.noreply.github.com>
2021-08-31 23:00:30 +0800

    Add in basic firestore functions

--- app/src/main/java/my/edu/tarc/rewardreferralapp/ApplyClaimFragment.kt
+++ app/src/main/java/my/edu/tarc/rewardreferralapp/ApplyClaimFragment.kt
@@ -1,16 +1,23 @@
 package my.edu.tarc.rewardreferralapp
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import my.edu.tarc.rewardreferralapp.databinding.FragmentApplyClaimBinding
 import my.edu.tarc.rewardreferralapp.databinding.FragmentReferralInsuranceListingBinding
 
 class ApplyClaimFragment : Fragment() {
+    val db = Firebase.firestore
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,6 +47,23 @@ class ApplyClaimFragment : Fragment() {
     }
 
     private fun ApplyClaim(): Boolean{
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Lee",
+            "last" to "Ken",
+            "born" to 2000
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
         return true
     }
 
