05bcc35d530e2930ddab20ac5e5e2241fca22e35
Ji Ho <soojiho127@gmail.com>
2021-09-02 18:54:58 +0800

    Jiho change some design

--- app/src/main/java/my/edu/tarc/rewardreferralapp/RewardCenterFragment.kt
+++ app/src/main/java/my/edu/tarc/rewardreferralapp/RewardCenterFragment.kt
@@ -1,10 +1,16 @@
 package my.edu.tarc.rewardreferralapp
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import my.edu.tarc.rewardreferralapp.data.Reward
+import my.edu.tarc.rewardreferralapp.data.RewardCenterAdapter
+import my.edu.tarc.rewardreferralapp.databinding.FragmentRewardCenterBinding
 
 
 class RewardCenterFragment : Fragment() {
@@ -13,8 +19,67 @@ class RewardCenterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reward_center, container, false)
+
+        val rewardList : List <Reward> = listOf(
+        Reward("RW0001","MyBee Umbrella","Umbrella from MyBee Company",500,
+            "22/08/2021","22/08/2022",50),
+
+            Reward("RW0002","MyBee Bottle","Bottle from MyBee Company",200,
+                "22/08/2021","26/08/2022",10),
+
+            Reward("RW0003","MyBee Wallet","Wallet from MyBee Company",300,
+                "22/08/2021","29/08/2022",0)
+        )
+
+        val binding:FragmentRewardCenterBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_reward_center, container, false)
+
+        val RewardAdapter = RewardCenterAdapter(rewardList,RewardCenterAdapter.ClaimListener{
+            rewardID,rewardName,PointNeeded,Stock -> val it = view
+            if (it != null) {
+                val builder = AlertDialog.Builder(activity)
+
+
+                if(Stock != 0) {
+
+                    builder.setTitle("Confirm Claim")
+                    builder.setMessage("Are you sure want to claim the reward $rewardName $rewardID $PointNeeded $Stock")
+
+                    builder.setPositiveButton("Yes") { dialogInterface, which ->
+                        Toast.makeText(context, "clicked yes", Toast.LENGTH_LONG).show()
+                    }
+
+                    builder.setNegativeButton("No") { dialogInterface, which ->
+                        dialogInterface.dismiss()
+                    }
+
+                }else{
+
+                    builder.setTitle("Out of stock")
+                    builder.setMessage("This reward already out of stock")
+
+                    builder.setNegativeButton("Back") { dialogInterface, which ->
+                        dialogInterface.dismiss()
+                    }
+
+                }
+
+                val alertDialog: AlertDialog = builder.create()
+                // Set other dialog properties
+                alertDialog.setCancelable(false)
+                alertDialog.show()
+
+            }
+        })
+
+        binding.RewardCenterRV.adapter = RewardAdapter
+        binding.RewardCenterRV.setHasFixedSize(true)
+
+        binding.btnRCSearch.setOnClickListener(){
+
+        }
+
+
+        return binding.root
     }
 
 }
--- app/src/main/java/my/edu/tarc/rewardreferralapp/RewardListingFragment.kt
+++ app/src/main/java/my/edu/tarc/rewardreferralapp/RewardListingFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import my.edu.tarc.rewardreferralapp.data.Reward
+import my.edu.tarc.rewardreferralapp.data.RewardListAdapter
+import my.edu.tarc.rewardreferralapp.databinding.FragmentRewardListingBinding
 
 
 class RewardListingFragment : Fragment() {
@@ -13,8 +18,30 @@ class RewardListingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reward_listing, container, false)
+        val rewardList : List <Reward> = listOf(
+            Reward("RW0001","MyBee Umbrella","Umbrella from MyBee Company",500,
+                "22/08/2021","22/08/2022",50),
+
+            Reward("RW0002","MyBee Bottle","Bottle from MyBee Company",200,
+                "22/08/2021","26/08/2022",10),
+
+            Reward("RW0003","MyBee Wallet","Wallet from MyBee Company",300,
+                "22/08/2021","29/08/2022",0)
+        )
+
+        val binding: FragmentRewardListingBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_reward_listing, container, false)
+
+        val rewardAdapter = RewardListAdapter(rewardList,RewardListAdapter.ViewListener{
+            rewardID -> val it = view
+            if (it != null) {
+                Toast.makeText(context, "$rewardID list", Toast.LENGTH_LONG).show()
+            }
+        })
+        binding.RewardListRV.adapter = rewardAdapter
+        binding.RewardListRV.setHasFixedSize(true)
+
+        return binding.root
+
     }
 
 }
